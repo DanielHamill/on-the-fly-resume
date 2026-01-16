3488ec8ccb28eabf5ab2710c4394709f50c00ea5
Daniel Hamill <danielhamill00@gmail.com>
2026-01-16 16:15:04 -0500

    added jinja for templating

---
+++ on_the_fly_resume/__init__.py
@@ -0,0 +1,7 @@
+"""on_the_fly_resume package
+
+Expose package utilities at import-time.
+"""
+from .scripts import compile_latex
+
+__all__ = ["compile_latex"]
---
+++ on_the_fly_resume/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    """Simple __main__ to demonstrate package run."""
+    print("on_the_fly_resume package. Use on_the_fly_resume.compile_latex() to build.")
+
+if __name__ == "__main__":
+    main()
